613fd77b73f96641e6e6738cbd594ed867029010
Monique Melo <71858557+moniquemelo@users.noreply.github.com>
2022-05-05 01:23:32 -0300

    Correções, Padronizações, Adição do Sobre

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top', //movimentar de cima para baixo
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content
+`)
