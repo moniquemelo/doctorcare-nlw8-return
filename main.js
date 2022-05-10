4ebd0611e7900b17b5369fcc2f8f8eaeae325ea5
Monique Melo <71858557+moniquemelo@users.noreply.github.com>
2022-05-09 22:07:11 -0300

    Adição Responsividade, CSS Grid, CSS Flex

--- main.js
+++ main.js
@@ -7,6 +7,37 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // Verificar se a seção passou da linha
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // Verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // Limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 // Mostrar o Navigation
@@ -26,10 +57,12 @@ function showBackToTopButtonOnScroll() {
   }
 }
 
+// Abrir o menu
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 
+// Fechar o menu
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
